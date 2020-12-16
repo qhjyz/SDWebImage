2a37d752988aed44337b323b16fdeeb12aaeadab
xxxoo <xxxoo>
2020-12-15 22:08:01 -0800

    could publish

---
+++ libSDWebImage/libSDWebImage.h
@@ -0,0 +1,13 @@
+//
+//  libSDWebImage.h
+//  libSDWebImage
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 Dailymotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libSDWebImage : NSObject
+
+@end
---
+++ libSDWebImage/libSDWebImage.m
@@ -0,0 +1,13 @@
+//
+//  libSDWebImage.m
+//  libSDWebImage
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 Dailymotion. All rights reserved.
+//
+
+#import "libSDWebImage.h"
+
+@implementation libSDWebImage
+
+@end
